3da328cd42d53137236d42f55fa4ea81d62fa462
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-06-01 13:22:12 -0300

    aula -9- databindign one way

--- front-end/guiaclientes/src/components/Cliente.vue
+++ front-end/guiaclientes/src/components/Cliente.vue
@@ -1,22 +1,31 @@
 <template>
     <div id="cliente">
-        <h2>Cliente -Componente AQUI!!! </h2>   
-        <h3>Descrição do Cliente</h3> 
-        {{  }}
+        
+        <!-- <input type="text" :value="nome" name="" id="">  usado para referenciar uma variável dentro do campos de textos-->
+
+        <input type="text" :value="descricao">
+        <h2>Cliente :{{ nome }} </h2>   
+        <h3>Descrição do Cliente: <p> {{descricao}} </p> </h3> 
+        {{ 2 + 2 * 4 }}
     </div>
 
 </template>
 
 <script>
     export default {
-
+        data(){
+            return {
+                nome: "Aldo",
+                numero: "",
+                email: "",
+                iddade: "",
+                descricao: "lorem IPSUM E TAL"
+            }
+        }
     }
 
 </script>
 
 <style scoped>
-    #cliente {
-        color: blue;
-        background-color: tomato;
-    }
+  
 </style>
